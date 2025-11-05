0a8f59a6ef578b25f5cbd9cbd4ed9c691560b2f2
anhmuonvn <anhlx2k@gmail.com>
2025-11-05 16:50:07 +0700

    tao cv va hien thi cv

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\JobType;
+use App\Models\Job;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -153,6 +156,70 @@ class AccountController extends Controller
     {
         Auth::logout();
         return redirect()->route('account.login');
+    }
+    public function createJob()
+    {
+        $jobTypes = JobType::orderBy('name','asc')->where('status',1)->get();
+        $categories = Category::orderBy('name','asc')->where('status',1)->get();
+        return view('front.account.job.create',
+    ['jobTypes'=>$jobTypes,'categories'=>$categories]);
+    }
+    public function saveJob(Request $request)
+    {
+        $rules = [
+            'title'=> 'required|min:5|max:200',
+            'category'=> 'required',
+            'jobType'=> 'required',
+            'vacancy'=> 'required|integer',
+            'location'=> 'required|max:50',
+            'description'=> 'required',
+            'experience'=> 'required',
+            'company_name'=> 'required|min:3|max:75',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if( $validator->passes() )
+        {
+            $job = new Job(); 
+            $job->title = $request->title;
+            $job->category_id = $request->category;
+            $job->user_id = Auth::user()->id;
+            $job->job_type_id = $request->category;
+            $job->vacancy = $request->vacancy;
+            $job->salary = $request->salary;
+            $job->location = $request->location;
+            $job->description = $request->description;
+            $job->benefits = $request->benefits;
+            $job->benefits = $request->benefits;
+            $job->responsibility = $request->responsibility;
+            $job->qualifications = $request->qualifications;
+            $job->keywords = $request->keywords;
+            $job->experience = $request->experience;
+            $job->company_location = $request->company_location;
+            $job->company_name = $request->company_name;
+            $job->company_website = $request->company_website;
+            $job->save();
+            session()->flash('success','Them cong viec thanh cong');
+            return response()->json([
+                'status'=>true,
+                'errors'=>[]
+            ]);
+        } 
+        else
+        {
+            return response()->json([
+                'status'=>false,
+                'errors'=>$validator->errors()
+            ]);
+        } 
+
+    }
+    public function myJobs()
+    {
+        $jobs = Job::where('user_id', Auth::user()->id)->with('jobType')
+        ->paginate(5);
+        // dd($jobs);  
+        return view('front.account.job.my-jobs',
+        ['jobs'=>$jobs]);
     }
     
 }
